c0947ec079b530a155393bea84a1fb813de9111a
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-23 20:04:03 +0200

    fixes navigation functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
 import './css/header.css'
 
 
 const Header = () => {
-        return (
-             <nav>
-                <div className="title">My Portfolio</div>
-             </nav>
-        )   
+  return (
+    <nav className="header">
+      <h1 className="header-title">
+        <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
+      </h1>
+      <ul className="list-reset flex">
+        <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/about'>About Me</NavLink></li>
+        <li className="mr-6"><NavLink className="text-white hover:text-grey no-underline" activeStyle={{fontWeight: "bold"}} to='/projects'>My Projects</NavLink></li>
+      </ul>
+    </nav>
+        ) 
 }
 
 export default Header
