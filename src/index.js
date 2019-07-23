bbc9cc3b021dacfab03df6067172d1e303403d40
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-23 19:55:55 +0200

    initializes switch and routes and adds about and projects sections

--- src/index.js
+++ src/index.js
@@ -3,16 +3,32 @@ import ReactDOM from "react-dom"
 import LandingPage from "./LandingPage"
 import Header from './Header'
 import Footer from './Footer'
+import { BrowserRouter } from 'react-router-dom'
+import Projects from './Projects'
+import About from './About'
+import { Switch, Route } from 'react-router-dom'
 import './css/index.css'
 
 const App = () => {
   return (
-    <div>
-				<Header />
-        <LandingPage />
-				<Footer />
-    </div>
-	)
+    <div className="page-wrapper">
+        <div className="page-content">
+            <Header />
+            <div className="content-wrapper">
+                <Switch>
+                    <Route exact path='/' component={LandingPage}></Route>
+                    <Route exact path='/about' component={About}></Route>
+                    <Route exact path='/projects' component={Projects}></Route>
+                </Switch>
+            </div>
+            <Footer />
+        </div >
+    </div >
+  )
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render((
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+), document.getElementById('app'));
