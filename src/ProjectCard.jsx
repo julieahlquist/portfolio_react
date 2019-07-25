75499dbbe19a3ff3e78b4964e730c6c0af700100
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-25 14:14:50 +0200

    minor styling changes

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -7,14 +7,13 @@ const ProjectCard = (props) => {
         <div key={project.id} className="pr-id">
 
             <div className="pr-card" style={{ minHeight: '150px' }} >
-                <div className="pr-title">{project.name}
+                <div className="prc-title">{project.name}</div>
                 <p className="prc-text">
                     {project.description}
                 </p>
             <button className="pr-button">
                 Link
             </button>
-            </div>
             </div>
         </div>
     )
