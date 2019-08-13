8222a3f9b597791525dc87fcdeb6a7477510c772
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-13 17:17:23 +0200

    changes about me text and font colors and styling for project cards

--- src/About.jsx
+++ src/About.jsx
@@ -2,19 +2,20 @@ import React from "react"
 import './css/about.css'
 
 const About = () => {
-    return (
-        <div>
-            <div className="ab-title">About Me</div>
-            <div className="ab-text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                Mollitia temporibus, suscipit vero, tempore voluptatem assumenda 
-                odio ipsam dignissimos exercitationem dicta possimus, quod nemo 
-                aliquid minima tempora itaque labore porro rem. Lorem ipsum dolor 
-                sit amet consectetur adipisicing elit. Eius eaque eligendi natus 
-                corrupti quibusdam voluptate dolorem autem magnam. Possimus est 
-                quia cumque eos quae earum a! Inventore soluta iste laborum?</div>
-        </div>
-    )
+	return (
+      <div>
+          <div className="ab-title">About Me</div>
+          <div className="ab-text">
+					My name is Julie Ahlquist and I’m a soon-to-be fullstack 
+					junior developer, currently attending a coding bootcamp at
+					Craft Academy in Stockholm. </div>
+					<div className="ab-text">
+          I’m coding in Ruby and JavaScript language, building 
+					applications primarily using Rails and/or React frameworks. 
+					Agile mindset and TDD/BDD is key, and I’m also practising 
+          version control and coverage metrics.</div>
+      </div>
+  )
 };
 
 export default About
