268941c0841a2bc2302bace2e88ac9e8a7c2e193
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-14 21:58:16 +0200

    opens social media links in new tabs

--- src/SocialMedia.jsx
+++ src/SocialMedia.jsx
@@ -6,25 +6,25 @@ const SocialMedia = () => {
 	return (
 		<ul>
   		<li>
-    		<a href="https://github.com/julieahlquist">
+    		<a href="https://github.com/julieahlquist" target="_blank">
 				<FontAwesomeIcon icon={['fab', 'github']} />
       		<span> - Github</span>
     		</a>
   		</li>
   		<li>
-    		<a href="https://www.linkedin.com/in/julieahlquist/">
+    		<a href="https://www.linkedin.com/in/julieahlquist/" target="_blank">
 				<FontAwesomeIcon icon={['fab', 'linkedin-in']} />
       		<span> - Linkedin</span>
     		</a>
   		</li>
   		<li>
-    		<a href="https://www.instagram.com/julieahlquist/?hl=sv">
+    		<a href="https://www.instagram.com/julieahlquist/?hl=sv" target="_blank">
 				<FontAwesomeIcon icon={['fab', 'instagram']} />
       		<span> - Instagram</span>
     		</a>
   		</li>  
 			<li>
-    		<a href="https://www.facebook.com/julie.ahlquist">
+    		<a href="https://www.facebook.com/julie.ahlquist" target="_blank">
 				<FontAwesomeIcon icon={['fab', 'facebook-f']} />
       		<span> - Facebook</span>
   			</a>
